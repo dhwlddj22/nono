163ab149a3397afad77f5af79c4443823c2d2de4
LCH_4510 <lch933804614@gmail.com>
2025-04-30 12:46:04 +0900

    회원가입 후 이전버튼을 누르면 바로 종료되도록 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:flutter/services.dart';
 
 import 'screens/login/login_screen.dart';
 import 'screens/main_screen.dart';
@@ -21,26 +22,32 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Pretendard',
-        brightness: Brightness.dark,
-      ),
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(
-              backgroundColor: Colors.black,
-              body: Center(child: CircularProgressIndicator(color: Colors.green)),
-            );
-          }
-          if (snapshot.hasData) {
-            return MainScreen();
-          }
-          return LoginScreen();
-        },
+    return WillPopScope(
+      onWillPop: () async {
+        SystemNavigator.pop();
+        return false;
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          fontFamily: 'Pretendard',
+          brightness: Brightness.dark,
+        ),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Scaffold(
+                backgroundColor: Colors.black,
+                body: Center(child: CircularProgressIndicator(color: Colors.green)),
+              );
+            }
+            if (snapshot.hasData) {
+              return MainScreen();
+            }
+            return LoginScreen();
+          },
+        ),
       ),
     );
   }
